cfa552f10fa3769bf1d20d2dad038a5aec96ec50
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-14 18:16:02 +0200

    frontend -> adding jsx to DisplayPprofile component

--- physiocare_frontend/components/displayProfile/DisplayProfile.js
+++ physiocare_frontend/components/displayProfile/DisplayProfile.js
@@ -1,1 +1,10 @@
 import { StyleSheet, View, Text } from "react-native";
+
+function ProfileDisplay(props) {
+  return (
+    <View style={styles.profileInputContainer}>
+      <Text style={styles.inputTitle}>{props.inputTitle}</Text>
+      <Text style={styles.textInfo}>Neuromusculoskeletal</Text>
+    </View>
+  );
+}
