2ad9b1e2b93db5313b599447e9dc87ce8fd862ee
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-14 18:38:26 +0200

    frontend -> styling TimeInput component

--- physiocare_frontend/components/timeInput/TimeInput.js
+++ physiocare_frontend/components/timeInput/TimeInput.js
@@ -11,3 +11,14 @@ function TimeInput(props) {
 }
 
 export default TimeInput;
+
+const styles = StyleSheet.create({
+  TimeTextInput: {
+    width: "43%",
+    backgroundColor: "#FFFFFF",
+    height: 32,
+    borderRadius: 10,
+    marginBottom: 16,
+    textAlign: "center",
+  },
+});
