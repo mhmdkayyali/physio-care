8875805c723aa7c3b25c238b76ae3667d72b8227
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-14 18:20:28 +0200

    frontend -> adding jsx to ProfileInput component

--- physiocare_frontend/components/profileInput/ProfileInput.js
+++ physiocare_frontend/components/profileInput/ProfileInput.js
@@ -1,1 +1,17 @@
 import { TextInput, StyleSheet, View, Text } from "react-native";
+
+function ProfileInput(props) {
+  return (
+    <View style={styles.profileInputContainer}>
+      <Text style={styles.inputTitle}>{props.inputTitle}</Text>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={(value) => {
+          props.changeInput(value);
+        }}
+        editable={props.canEdit}
+        value={props.value}
+      />
+    </View>
+  );
+}
