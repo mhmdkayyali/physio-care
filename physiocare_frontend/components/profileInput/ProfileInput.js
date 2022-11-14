3e420d989b0dab28cf4701d7ee76d9a1e8f70516
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-14 18:21:19 +0200

    frontend -> styling ProfileInput

--- physiocare_frontend/components/profileInput/ProfileInput.js
+++ physiocare_frontend/components/profileInput/ProfileInput.js
@@ -17,3 +17,24 @@ function ProfileInput(props) {
 }
 
 export default ProfileInput;
+
+const styles = StyleSheet.create({
+  profileInputContainer: {
+    width: "100%",
+    borderBottomWidth: 1,
+    marginVertical: 16,
+    borderColor: "#A1A1A1",
+  },
+  textInput: {
+    width: "100%",
+    fontSize: 20,
+    borderRadius: 5,
+    paddingLeft: 12,
+    color: "#383838",
+  },
+  inputTitle: {
+    color: "#A1A1A1",
+    fontSize: 12,
+    paddingLeft: 12,
+  },
+});
