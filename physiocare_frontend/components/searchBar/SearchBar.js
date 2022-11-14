c27b1b332bfeec9d74045497a58ed8110f322ace
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-14 15:39:04 +0200

    frontend -> adding jsx to searchbar

--- physiocare_frontend/components/searchBar/SearchBar.js
+++ physiocare_frontend/components/searchBar/SearchBar.js
@@ -1,1 +1,11 @@
 import { TextInput, StyleSheet } from "react-native";
+
+function SearchingBar(props) {
+  return (
+    <TextInput
+      style={styles.searchInput}
+      placeholder={props.placeHolder}
+      onChangeText={props.onChangeHandler}
+    ></TextInput>
+  );
+}
