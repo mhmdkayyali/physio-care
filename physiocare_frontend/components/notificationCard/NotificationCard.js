310f4ed165907c25ddb478a0554a9d34d7f20510
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-14 18:00:02 +0200

    frontend -> adding jsx to notification card

--- physiocare_frontend/components/notificationCard/NotificationCard.js
+++ physiocare_frontend/components/notificationCard/NotificationCard.js
@@ -1,1 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
+
+function NotificationCard(props) {
+  return (
+    <View style={styles.cardContainer}>
+      <View style={styles.cardDetailsContainer}>
+        <View style={styles.nameTime}>
+          <Text style={styles.cardDetailsNames}>{props.userName}</Text>
+          <Text>{props.receivedTime}</Text>
+        </View>
+        <Text style={styles.cardDetails}>{props.notificationMessage}</Text>
+      </View>
+    </View>
+  );
+}
