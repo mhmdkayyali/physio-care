149ac69a97dd0aab0a99c86ad8076497dd9b127f
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-14 18:23:06 +0200

    frontend -> adding jsx to ProfilePicture component

--- physiocare_frontend/components/profilePicture/ProfilePicture.js
+++ physiocare_frontend/components/profilePicture/ProfilePicture.js
@@ -1,1 +1,12 @@
 import { StyleSheet, Image, View } from "react-native";
+
+function ProfilePicture() {
+  return (
+    <View style={styles.profilePictureContainer}>
+      <Image
+        style={styles.profilePicture}
+        source={require("../assets/images/doctor.jpg")}
+      />
+    </View>
+  );
+}
