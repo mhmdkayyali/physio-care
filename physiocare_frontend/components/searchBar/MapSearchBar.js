1863cd02372c7d0ddcc10d1d096d76203e0f88db
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-14 15:35:11 +0200

    frontend -> adding style to mapsearchbar

--- physiocare_frontend/components/searchBar/MapSearchBar.js
+++ physiocare_frontend/components/searchBar/MapSearchBar.js
@@ -5,3 +5,15 @@ function MapSearchBar(props) {
 }
 
 export default MapSearchBar;
+
+const styles = StyleSheet.create({
+  textInput: {
+    width: "100%",
+    backgroundColor: "#FFFFFF",
+    height: 40,
+    borderRadius: 10,
+    borderWidth: 0.5,
+    borderColor: "#C0C0C0",
+    textAlign: "center",
+  },
+});
