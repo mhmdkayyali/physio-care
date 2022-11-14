a88583aeb3db033994e39560535da42fa40ed6b6
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-14 13:04:26 +0200

    frontend -> adding jsx to button.jsx

--- physiocare_frontend/components/button/button.jsx
+++ physiocare_frontend/components/button/button.jsx
@@ -1,1 +1,11 @@
 import { StyleSheet, View, Pressable, Text } from "react-native";
+
+function Btn(props) {
+  return (
+    <Pressable style={btnStyle(props.btnStyle)} onPress={props.onPress}>
+      <View>
+        <Text style={textStyle(props.textStyle)}>{props.btnText}</Text>
+      </View>
+    </Pressable>
+  );
+}
