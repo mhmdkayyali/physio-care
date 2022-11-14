eaa578b0445310c31518ccd9516377599f00e9a7
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-14 18:30:53 +0200

    frontend -> adding jsx to AppointmentCard component

--- physiocare_frontend/components/appointmentCard/AppointmentCard.js
+++ physiocare_frontend/components/appointmentCard/AppointmentCard.js
@@ -1,2 +1,32 @@
 import { StyleSheet, Text, View } from "react-native";
 import Buttons from "../button/Buttons";
+
+function AppointmentCard(props) {
+  return (
+    <View style={styles.cardContainer}>
+      <View style={styles.cardInformation}>
+        <View style={styles.lineContainer}>
+          <Text style={styles.cardDetailsNames}>{props.userName}</Text>
+          <Text style={styles.cardDetails}>{props.conditionSpecialty}</Text>
+        </View>
+        <View style={styles.lineContainer}>
+          <Text style={styles.cardDetails}>{props.date}</Text>
+          <Text style={styles.cardDetails}>{props.time}</Text>
+        </View>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Buttons
+          btnStyle={"attendedBtn"}
+          textStyle={"scheduleBtnText"}
+          btnText={"ATTENDED"}
+        />
+        <Buttons
+          btnStyle={"cancelBtn"}
+          textStyle={"scheduleBtnText"}
+          btnText={"CANCEL"}
+          onPress={props.showModal}
+        />
+      </View>
+    </View>
+  );
+}
