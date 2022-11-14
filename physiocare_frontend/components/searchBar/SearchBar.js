50de1e8e3c5d9d13dec13cde9eaccbed3c0e32b3
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-14 14:44:00 +0200

    frontend -> exporting searchbar

--- physiocare_frontend/components/searchBar/SearchBar.js
+++ physiocare_frontend/components/searchBar/SearchBar.js
@@ -3,3 +3,5 @@ import { TextInput, StyleSheet } from "react-native";
 function SearchBar(props) {
   return <TextInput style={styles.textInput} placeholder="Search"></TextInput>;
 }
+
+export default SearchBar;
