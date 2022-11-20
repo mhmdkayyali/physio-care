47096cad7f7145859c406c0d3ab95d27d29cd21a
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-20 14:43:07 +0200

    frontend -> adding customDrawer components to CustomDrawer.js

--- physiocare_frontend/components/customDrawer/CustomDrawer.js
+++ physiocare_frontend/components/customDrawer/CustomDrawer.js
@@ -5,3 +5,13 @@ import {
 } from "@react-navigation/drawer";
 import { Ionicons } from "@expo/vector-icons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+function CustomDrawer(props) {
+  return (
+    <>
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+    </>
+  );
+}
