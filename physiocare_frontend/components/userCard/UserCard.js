468776e33c953f67fb4283dcf2551e1736f9d9b9
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-20 14:17:53 +0200

    frontend -> adding jsx to UserCard.js

--- physiocare_frontend/components/userCard/UserCard.js
+++ physiocare_frontend/components/userCard/UserCard.js
@@ -1,2 +1,17 @@
 import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+
+function UserCard(props) {
+  const navigation = useNavigation();
+  return (
+    <Pressable style={styles.cardContainer}>
+      <View style={styles.cardDetailsContainer}>
+        <Text style={styles.cardDetailsNames}>{props.userName}</Text>
+        <Text style={styles.cardDetails}>{props.conditionSpecialty}</Text>
+        <Text style={styles.cardDetails}>{props.location}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+export default UserCard;
