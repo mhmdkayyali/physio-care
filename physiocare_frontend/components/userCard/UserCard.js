cbbb5412e601c9ff606e5647c6ba1341392fd2ee
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-20 14:19:06 +0200

    frontend -> adding navigation logic to UserCard.js

--- physiocare_frontend/components/userCard/UserCard.js
+++ physiocare_frontend/components/userCard/UserCard.js
@@ -4,7 +4,10 @@ import { useNavigation } from "@react-navigation/native";
 function UserCard(props) {
   const navigation = useNavigation();
   return (
-    <Pressable style={styles.cardContainer}>
+    <Pressable
+      onPress={() => navigation.navigate("UserDetails", { user: props.user })}
+      style={styles.cardContainer}
+    >
       <View style={styles.cardDetailsContainer}>
         <Text style={styles.cardDetailsNames}>{props.userName}</Text>
         <Text style={styles.cardDetails}>{props.conditionSpecialty}</Text>
