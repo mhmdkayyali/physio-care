b89b80b004bcb6cd4461db2c9027a583d0a96dec
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-20 14:20:36 +0200

    frontend -> adding condition to check if user is male or female to UserCard.js

--- physiocare_frontend/components/userCard/UserCard.js
+++ physiocare_frontend/components/userCard/UserCard.js
@@ -8,6 +8,18 @@ function UserCard(props) {
       onPress={() => navigation.navigate("UserDetails", { user: props.user })}
       style={styles.cardContainer}
     >
+      <View style={styles.pictureContainer}>
+        {props.userType === "THERAPIST" ? (
+          <Image
+            style={styles.picture}
+            source={require("../assets/images/doctor.jpg")}
+          />
+        ) : (
+          <View
+            style={props.gender === "MALE" ? styles.male : styles.female}
+          ></View>
+        )}
+      </View>
       <View style={styles.cardDetailsContainer}>
         <Text style={styles.cardDetailsNames}>{props.userName}</Text>
         <Text style={styles.cardDetails}>{props.conditionSpecialty}</Text>
