468dc816f1bb3816cb82b012a464d27cbf5cd738
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 23:35:09 +0200

    frontend -> importing searchingbar and Button components in Appointment.js

--- physiocare_frontend/screens/appointment/Appointment.js
+++ physiocare_frontend/screens/appointment/Appointment.js
@@ -1,3 +1,5 @@
 import { View, StyleSheet, ScrollView, Modal, Text } from "react-native";
 import { useEffect, useState } from "react";
 import ScheduleCard from "../components/ScheduleCard";
+import SearchingBar from "../components/SearchingBar";
+import Btn from "../components/Btn";
