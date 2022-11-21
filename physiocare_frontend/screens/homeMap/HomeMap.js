fa45cd72cab180a996f1e5d684f9312ef1124a7a
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 16:26:00 +0200

    frontend -> importing axios from axios in HomeMap.js

--- physiocare_frontend/screens/homeMap/HomeMap.js
+++ physiocare_frontend/screens/homeMap/HomeMap.js
@@ -3,4 +3,5 @@ import { View, StyleSheet, Text, Dimensions } from "react-native";
 import { useState, useEffect } from "react";
 import MapView, { Callout, Circle, Marker } from "react-native-maps";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
 import Buttons from "../../components/button/Buttons";
