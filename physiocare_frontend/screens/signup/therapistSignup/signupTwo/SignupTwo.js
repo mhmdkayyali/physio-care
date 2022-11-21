8d18eb366f2df22f94f3b900d883ef4008cfde73
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 13:51:51 +0200

    frontend -> importing useState, useEffect and asyncStorage in SignupTwo.js

--- physiocare_frontend/screens/signup/therapistSignup/signupTwo/SignupTwo.js
+++ physiocare_frontend/screens/signup/therapistSignup/signupTwo/SignupTwo.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
 import UserTextInput from "../../../components/UserTextInput";
 import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import Buttons from "../../../components/Buttons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
