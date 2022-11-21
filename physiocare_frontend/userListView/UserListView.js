ec4e7f21e6333d838f37c64b7ca49a164398004a
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 17:27:22 +0200

    importing useState and useEffect from react in UserListView.js

--- physiocare_frontend/userListView/UserListView.js
+++ physiocare_frontend/userListView/UserListView.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { View, StyleSheet, ScrollView, Text } from "react-native";
 import UserCard from "../../components/UserCard";
 import SearchingBar from "../../components/SearchingBar";
