43ea49e8e8ad1cb5605653d058e2c16f7282b81d
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 10:30:03 +0200

    frontend -> adding jsx to SignupOne.js

--- physiocare_frontend/screens/signup/signupOne/SignupOne.js
+++ physiocare_frontend/screens/signup/signupOne/SignupOne.js
@@ -1,3 +1,35 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import Buttons from "../../components/Buttons";
+
+function SignupOne() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.logo_login_container}>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../../assets/images/logo.png")}
+          />
+        </View>
+        <Text style={styles.title}>Sign up</Text>
+      </View>
+      <View style={styles.userTypesContainer}>
+        <View style={styles.paragraphContainer}>
+          <Text style={styles.paragraph}>What type of user are you?</Text>
+        </View>
+        <Buttons />
+        <Buttons />
+      </View>
+      <View style={styles.btnContainer}>
+        <Buttons />
+        <View style={styles.account_login}>
+          <Text>Already have an account? </Text>
+          <Pressable>
+            <Text style={styles.loginBtn}>Login</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
+}
