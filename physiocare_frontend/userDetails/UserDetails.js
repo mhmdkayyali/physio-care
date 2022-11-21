ba2ba1b8bf093a65a2b4b6c7b1bcda8eda214324
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 19:31:36 +0200

    frontend -> importing profilePicture and ProfileDisplay in UserDetails.js

--- physiocare_frontend/userDetails/UserDetails.js
+++ physiocare_frontend/userDetails/UserDetails.js
@@ -1,2 +1,4 @@
+import ProfileDisplay from "../../components/ProfileDisplay";
+import ProfilePicture from "../../components/ProfilePicture";
 import Btn from "../../components/Btn";
 import { View, StyleSheet, ScrollView, Text } from "react-native";
