51db93a2f39e73ecfd088fc21a7db620c890cdf0
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 21:00:49 +0200

    frontend -> adding jsx to ScheduleDay.js

--- physiocare_frontend/screens/bookingSession/scheduleDay/ScheduleDay.js
+++ physiocare_frontend/screens/bookingSession/scheduleDay/ScheduleDay.js
@@ -2,3 +2,26 @@ import { StyleSheet, View, Text } from "react-native";
 import Buttons from "../../components/Buttons";
 import ProfilePicture from "../../components/ProfilePicture";
 import { Calendar } from "react-native-calendars";
+
+function SchedulingDay({ navigation, route }) {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.infoDateContainer}>
+        <View style={styles.profilePictureContainer}>
+          <ProfilePicture />
+        </View>
+        <View style={styles.nameSpecialtyContainer}>
+          <Text style={styles.therapistNameText}>Mohammad Al Kayyali</Text>
+          <Text style={styles.specialtyText}>Neuromusculoskeletal</Text>
+        </View>
+        <Buttons btnStyle={"callButton"} btnText={"CALL NOW"} />
+      </View>
+      <View style={styles.scheduleTimeBtnContainer}>
+        <View style={styles.scheduleTimeContainer}>
+          <Text style={styles.scheduleParagraph}>Schedule a session</Text>
+          <Text style={styles.selectTimeParagraph}>select a time</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
