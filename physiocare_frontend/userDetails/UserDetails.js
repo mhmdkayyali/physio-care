5eedf7203ab304f86665f8c215e4d0e399dfbbe2
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 19:32:20 +0200

    frontend -> adding jsx to UserDetails.js

--- physiocare_frontend/userDetails/UserDetails.js
+++ physiocare_frontend/userDetails/UserDetails.js
@@ -2,3 +2,24 @@ import ProfileDisplay from "../../components/ProfileDisplay";
 import ProfilePicture from "../../components/ProfilePicture";
 import Btn from "../../components/Btn";
 import { View, StyleSheet, ScrollView, Text } from "react-native";
+
+function UserDetails({ navigation, route }) {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.profilePictureContainer}>
+        <ProfilePicture />
+      </View>
+      <View>
+        <Text style={styles.therapistNameText}></Text>
+      </View>
+      <ScrollView>
+        <ProfileDisplay inputTitle={"SPECIALITY"} />
+        <ProfileDisplay inputContent={user.location} inputTitle={"LOCATION"} />
+        <ProfileDisplay inputTitle={"RATE PER SESSION"} />
+      </ScrollView>
+      <View style={styles.btnContainer}>
+        <Btn btnText={"BOOK A SESSION"} />
+      </View>
+    </View>
+  );
+}
