87a51d84b55a13634a16e326f642bcf423885192
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 21:22:11 +0200

    frontend -> importing PressedTime component in ScheduleTime.js

--- physiocare_frontend/screens/bookingSession/scheduleTime/ScheduleTime.js
+++ physiocare_frontend/screens/bookingSession/scheduleTime/ScheduleTime.js
@@ -2,3 +2,4 @@ import { StyleSheet, View, Text } from "react-native";
 import { useState, useEffect } from "react";
 import Btn from "../../components/Btn";
 import ProfilePicture from "../../components/ProfilePicture";
+import PressedTime from "../../components/PressedTime";
