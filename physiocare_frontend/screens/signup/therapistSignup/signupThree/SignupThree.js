dfc933fe01a44d94e7a7907c4f3c5bae8e1ed6c7
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 14:30:45 +0200

    frontend -> adding function for navigation and sending data in SignupThree.js

--- physiocare_frontend/screens/signup/therapistSignup/signupThree/SignupThree.js
+++ physiocare_frontend/screens/signup/therapistSignup/signupThree/SignupThree.js
@@ -5,6 +5,15 @@ import MapView, { Callout, Circle, Marker } from "react-native-maps";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function SignupThree({ navigation }) {
+  const nextButtonHandler = () => {
+    const data2 = {
+      ...data,
+      ...pin,
+    };
+    storeData(data2);
+    navigation.navigate("SignupPageFourTherapist");
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.logo_login_container}>
