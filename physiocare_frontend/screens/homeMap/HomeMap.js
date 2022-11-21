a8ad8427cc651f9371ed5f722bc606b9a3dd59fa
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 16:25:01 +0200

    frontend -> importing mapView from react-native-maps in HomeMap.js

--- physiocare_frontend/screens/homeMap/HomeMap.js
+++ physiocare_frontend/screens/homeMap/HomeMap.js
@@ -1,4 +1,5 @@
 import * as React from "react";
 import { View, StyleSheet, Text, Dimensions } from "react-native";
 import { useState, useEffect } from "react";
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
 import Buttons from "../../components/button/Buttons";
