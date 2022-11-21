9fb506a8e52081fe1a3ee8e8cf532f4fd3b1c7c2
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 17:27:44 +0200

    frontend -> importing axios from axios in UserListView.js

--- physiocare_frontend/userListView/UserListView.js
+++ physiocare_frontend/userListView/UserListView.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import { View, StyleSheet, ScrollView, Text } from "react-native";
 import UserCard from "../../components/UserCard";
 import SearchingBar from "../../components/SearchingBar";
