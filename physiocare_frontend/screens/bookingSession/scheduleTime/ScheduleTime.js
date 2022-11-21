f3ced3a8e4d39082fd42e68c67db8ae27fb38156
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 21:22:51 +0200

    frontend -> importing asyncStorage and scrollView in ScheduleTime.js

--- physiocare_frontend/screens/bookingSession/scheduleTime/ScheduleTime.js
+++ physiocare_frontend/screens/bookingSession/scheduleTime/ScheduleTime.js
@@ -3,3 +3,5 @@ import { useState, useEffect } from "react";
 import Btn from "../../components/Btn";
 import ProfilePicture from "../../components/ProfilePicture";
 import PressedTime from "../../components/PressedTime";
+import { ScrollView } from "react-native-gesture-handler";
+import AsyncStorage from "@react-native-async-storage/async-storage";
