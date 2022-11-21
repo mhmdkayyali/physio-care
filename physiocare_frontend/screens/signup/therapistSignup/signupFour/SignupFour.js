8757ab7c33126fe9ba4c83b2263db63b5b96dd99
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 15:37:26 +0200

    frontend -> importing asyncStorage in SignupFour.js

--- physiocare_frontend/screens/signup/therapistSignup/signupFour/SignupFour.js
+++ physiocare_frontend/screens/signup/therapistSignup/signupFour/SignupFour.js
@@ -2,3 +2,4 @@ import { useEffect, useState } from "react";
 import { StyleSheet, Text, View, Image, Pressable, Button } from "react-native";
 import Buttons from "../../../components/Buttons";
 import TimeInput from "../../../components/TimeInput";
+import AsyncStorage from "@react-native-async-storage/async-storage";
