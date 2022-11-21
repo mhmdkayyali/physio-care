12aa2148040aaa288437c088f05035a08c5e38d6
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 21:23:59 +0200

    frontend -> adding jsx to ScheduleTime.js

--- physiocare_frontend/screens/bookingSession/scheduleTime/ScheduleTime.js
+++ physiocare_frontend/screens/bookingSession/scheduleTime/ScheduleTime.js
@@ -7,3 +7,29 @@ import { ScrollView } from "react-native-gesture-handler";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import uuid from "react-native-uuid";
+
+function SchedulingTime({ navigation, route }) {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.infoDateContainer}>
+        <View style={styles.profilePictureContainer}>
+          <ProfilePicture />
+        </View>
+        <View style={styles.nameSpecialtyContainer}>
+          <Text style={styles.therapistNameText}>Mohammad Al Kayyali</Text>
+          <Text style={styles.specialtyText}>Neuromusculoskeletal</Text>
+        </View>
+        <View>
+          <Text style={styles.dayText}>{weekDays[currentDate]}</Text>
+          <Text style={styles.dateText}>{selectedDate}</Text>
+        </View>
+      </View>
+      <View style={styles.scheduleTimeBtnContainer}>
+        <View style={styles.scheduleTimeContainer}>
+          <Text style={styles.scheduleParagraph}>Schedule a session</Text>
+          <Text style={styles.selectTimeParagraph}>select a time</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
