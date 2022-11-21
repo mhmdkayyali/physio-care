6cdf9434c1b9dac5a9345df0a216d5f88a3fa61e
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 15:38:08 +0200

    frontend -> importing axios in SignupFour.js

--- physiocare_frontend/screens/signup/therapistSignup/signupFour/SignupFour.js
+++ physiocare_frontend/screens/signup/therapistSignup/signupFour/SignupFour.js
@@ -3,3 +3,4 @@ import { StyleSheet, Text, View, Image, Pressable, Button } from "react-native";
 import Buttons from "../../../components/Buttons";
 import TimeInput from "../../../components/TimeInput";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
