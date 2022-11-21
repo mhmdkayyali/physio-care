f007133fc72724b08d970900ae025b5500cb3304
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 12:59:47 +0200

    frontend -> importing buttons in SignupFour.js

--- physiocare_frontend/screens/signup/patientSignup/signupFour/SignupFour.js
+++ physiocare_frontend/screens/signup/patientSignup/signupFour/SignupFour.js
@@ -2,3 +2,4 @@ import MapView, { Callout, Circle, Marker } from "react-native-maps";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+import Buttons from "../../../components/Buttons";
