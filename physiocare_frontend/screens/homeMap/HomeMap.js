60e47b5be8ec3995152ea396991c1dd7a9372bf2
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 16:25:30 +0200

    frontend -> importing asyncStorage in HomeMap.js

--- physiocare_frontend/screens/homeMap/HomeMap.js
+++ physiocare_frontend/screens/homeMap/HomeMap.js
@@ -2,4 +2,5 @@ import * as React from "react";
 import { View, StyleSheet, Text, Dimensions } from "react-native";
 import { useState, useEffect } from "react";
 import MapView, { Callout, Circle, Marker } from "react-native-maps";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import Buttons from "../../components/button/Buttons";
