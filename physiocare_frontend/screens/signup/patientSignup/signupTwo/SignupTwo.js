88b668dc35775933cdd2139466185430903ccbe2
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 11:06:31 +0200

    frontend -> importing scrollView in SignupTwo.js

--- physiocare_frontend/screens/signup/patientSignup/signupTwo/SignupTwo.js
+++ physiocare_frontend/screens/signup/patientSignup/signupTwo/SignupTwo.js
@@ -1,3 +1,4 @@
 import UserTextInput from "../../../components/UserTextInput";
 import { Text, View, Image } from "react-native";
 import Buttons from "../../../components/Buttons";
+import { ScrollView } from "react-native-gesture-handler";
