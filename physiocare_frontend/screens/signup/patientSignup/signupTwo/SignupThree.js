54dcffb5c9e9970b0907c0f626493978fcd69142
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 11:56:50 +0200

    frontend -> importing components and adding jsx to SignupThree.js

--- physiocare_frontend/screens/signup/patientSignup/signupTwo/SignupThree.js
+++ physiocare_frontend/screens/signup/patientSignup/signupTwo/SignupThree.js
@@ -1,2 +1,31 @@
+import { useEffect, useState } from "react";
+import UserTextInput from "../../../components/UserTextInput";
 import { StyleSheet, Text, View, Image, Pressable, Button } from "react-native";
+import Buttons from "../../../components/Buttons";
 import { ScrollView } from "react-native-gesture-handler";
+
+function SignupThree({ navigation, route }) {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.logo_login_container}>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../../../assets/images/logo.png")}
+          />
+        </View>
+        <Text style={styles.title}>Sign up</Text>
+      </View>
+      <ScrollView style={styles.inputContainer}>
+        <UserTextInput />
+        <UserTextInput />
+        <UserTextInput />
+        <UserTextInput />
+        <UserTextInput />
+      </ScrollView>
+      <View style={styles.btnContainer}>
+        <Buttons />
+      </View>
+    </View>
+  );
+}
