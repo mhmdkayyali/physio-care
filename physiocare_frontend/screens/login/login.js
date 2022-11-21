570f42976a4d8aad7aca3921990357c28f837b27
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 09:47:05 +0200

    frontend -> adding jsx to login.js

--- physiocare_frontend/screens/login/login.js
+++ physiocare_frontend/screens/login/login.js
@@ -1,3 +1,32 @@
 import Buttons from "../components/Buttons";
 import UserTextInput from "../components/UserTextInput";
 import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+
+const Login = ({ navigation }) => {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.logo_login_container}>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../assets/images/logo.png")}
+          />
+        </View>
+        <Text style={styles.title}>Login</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <UserTextInput />
+        <UserTextInput />
+      </View>
+      <View style={styles.login_signup_container}>
+        <Buttons btnText={"LOGIN"} />
+        <View style={styles.account_sign_up}>
+          <Text>Don't have an account? </Text>
+          <Pressable>
+            <Text style={styles.signup_btn}>Sign up</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
+};
