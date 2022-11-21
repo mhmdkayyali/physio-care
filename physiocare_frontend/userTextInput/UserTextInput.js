9cc0e3322d3bc59bca089b8f29ef0b34ca1dfd0f
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 08:45:24 +0200

    frontend -> adding logic to UserTextInput.js

--- physiocare_frontend/userTextInput/UserTextInput.js
+++ physiocare_frontend/userTextInput/UserTextInput.js
@@ -1,1 +1,14 @@
 import { TextInput, StyleSheet } from "react-native";
+
+function UserTextInput(props) {
+  return (
+    <TextInput
+      style={styles.textInput}
+      placeholder={props.placeHolder}
+      onChangeText={props.onChangeHandler}
+      secureTextEntry={props.secureTextEntry}
+      autoCapitalize={props.autoCapitalize}
+      keyboardType={props.keyboardType}
+    ></TextInput>
+  );
+}
