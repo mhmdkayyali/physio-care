e34e5ed9336e29b320e7d3ee385d4cb638a87ea2
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 15:38:46 +0200

    frontend -> adding jsx to SignupFour.js

--- physiocare_frontend/screens/signup/therapistSignup/signupFour/SignupFour.js
+++ physiocare_frontend/screens/signup/therapistSignup/signupFour/SignupFour.js
@@ -4,3 +4,46 @@ import Buttons from "../../../components/Buttons";
 import TimeInput from "../../../components/TimeInput";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
+
+function SignupFour({ navigation }) {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.logo_login_container}>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../../../assets/images/logo.png")}
+          />
+        </View>
+        <Text style={styles.title}>Sign up</Text>
+      </View>
+      <View style={styles.userTypesContainer}>
+        <View>
+          <Text>Set available hours</Text>
+          <View style={styles.availableTimeContainer}>
+            <TimeInput placeHolder={"8:00"} />
+            <View>
+              <Text style={styles.dash}>-</Text>
+            </View>
+            <TimeInput placeHolder={"17:00"} />
+          </View>
+          <View>
+            <Text>Set available days</Text>
+            <View>
+              <Buttons />
+              <Buttons />
+              <Btn />
+              <Buttons />
+              <Buttons />
+              <Buttons />
+              <Buttons />
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={styles.btnContainer}>
+        <Btn btnText={"SIGN UP"} />
+      </View>
+    </View>
+  );
+}
