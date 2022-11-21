459eeed8029d500a46db156d9f06c0e5010bd539
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 17:28:52 +0200

    frontend -> adding jsx to UserListView.js

--- physiocare_frontend/userListView/UserListView.js
+++ physiocare_frontend/userListView/UserListView.js
@@ -4,3 +4,16 @@ import { View, StyleSheet, ScrollView, Text } from "react-native";
 import UserCard from "../../components/UserCard";
 import SearchingBar from "../../components/SearchingBar";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+function UserListView() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.searchBarContainer}>
+        <SearchingBar
+          onChangeHandler={searchBarInputHandler}
+          placeHolder={"Search bar"}
+        />
+      </View>
+    </View>
+  );
+}
