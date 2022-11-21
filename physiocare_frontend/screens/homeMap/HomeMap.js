68d02c9edeec80cf2785c17f71094948f5909157
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 16:26:36 +0200

    frontend -> adding jsx to HomeMap.jsx

--- physiocare_frontend/screens/homeMap/HomeMap.js
+++ physiocare_frontend/screens/homeMap/HomeMap.js
@@ -5,3 +5,13 @@ import MapView, { Callout, Circle, Marker } from "react-native-maps";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 import Buttons from "../../components/button/Buttons";
+
+function HomeMap({ navigation }) {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.viewBtnSearchBarContainer}>
+        <Buttons />
+      </View>
+    </View>
+  );
+}
