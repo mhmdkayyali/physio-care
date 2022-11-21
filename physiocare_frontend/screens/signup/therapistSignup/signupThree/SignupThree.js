9464667c98ca96c77a836ab0491c993b709c39c0
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 14:19:47 +0200

    frontend -> importing mapView and AsyncStorage in SignupThree.js

--- physiocare_frontend/screens/signup/therapistSignup/signupThree/SignupThree.js
+++ physiocare_frontend/screens/signup/therapistSignup/signupThree/SignupThree.js
@@ -1,3 +1,5 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import Buttons from "../../../components/Buttons";
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
+import AsyncStorage from "@react-native-async-storage/async-storage";
