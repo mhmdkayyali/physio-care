a6fcf5f60e9426e7092a8088b934c3ffa71b6b02
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 20:43:40 +0200

    frontend -> adding logic to ProfileInput in jsx to handle input

--- physiocare_frontend/profile/Profile.js
+++ physiocare_frontend/profile/Profile.js
@@ -59,30 +59,35 @@ function Profile() {
         <ProfileInput
           inputTitle={"FIRST NAME"}
           canEdit={canEdit}
+          changeInput={(value) => handleInputChange(value, "first_name")}
           value={info.first_name}
           defaultValue={storageData?.first_name}
         />
         <ProfileInput
           inputTitle={"LAST NAME"}
           canEdit={canEdit}
+          changeInput={(value) => handleInputChange(value, "last_name")}
           value={info.last_name}
           defaultValue={storageData?.last_name}
         />
         <ProfileInput
           inputTitle={"EMAIL"}
           canEdit={canEdit}
+          changeInput={(value) => handleInputChange(value, "email")}
           value={info.email}
           defaultValue={storageData?.email}
         />
         <ProfileInput
           inputTitle={"PHONE NUMBER"}
           canEdit={canEdit}
+          changeInput={(value) => handleInputChange(value, "phone_number")}
           value={info.phone_number}
           defaultValue={storageData?.phone_number}
         />
         <ProfileInput
           inputTitle={"DATE OF BIRTH"}
           canEdit={canEdit}
+          changeInput={(value) => handleInputChange(value, "dob")}
           value={info.dob}
           defaultValue={storageData?.dob}
         />
@@ -91,6 +96,7 @@ function Profile() {
             <ProfileInput
               inputTitle={"SPECIALITY"}
               canEdit={canEdit}
+              changeInput={(value) => handleInputChange(value, "specialty")}
               value={info.specialty}
               defaultValue={
                 storageData?.therapist_additional_informations.specialty
@@ -102,18 +108,23 @@ function Profile() {
             <ProfileInput
               inputTitle={"DIAGNOSIS"}
               canEdit={canEdit}
+              changeInput={(value) => handleInputChange(value, "diagnosis")}
               value={info.diagnosis}
               defaultValue={storageData?.pt_additional_informations.diagnosis}
             />
             <ProfileInput
               inputTitle={"CASE DATE"}
               canEdit={canEdit}
+              changeInput={(value) => handleInputChange(value, "case_date")}
               value={info.case_date}
               defaultValue={storageData?.pt_additional_informations.case_date}
             />
             <ProfileInput
               inputTitle={"TREATING DOCTOR"}
               canEdit={canEdit}
+              changeInput={(value) =>
+                handleInputChange(value, "treating_doctor")
+              }
               value={info.treating_doctor}
               defaultValue={
                 storageData?.pt_additional_informations.treating_doctor
