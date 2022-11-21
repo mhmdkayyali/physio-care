452514fcbb577d57b2474adf01becce0c1337082
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 13:52:49 +0200

    frontend -> adding jsx in SignupTwo.js

--- physiocare_frontend/screens/signup/therapistSignup/signupTwo/SignupTwo.js
+++ physiocare_frontend/screens/signup/therapistSignup/signupTwo/SignupTwo.js
@@ -3,3 +3,34 @@ import UserTextInput from "../../../components/UserTextInput";
 import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import Buttons from "../../../components/Buttons";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+function SignupTwo({ navigation }) {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.logo_login_container}>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../../../assets/images/logo.png")}
+          />
+        </View>
+        <Text style={styles.title}>Sign up</Text>
+      </View>
+      <ScrollView style={styles.inputContainer}>
+        <UserTextInput />
+        <UserTextInput />
+        <UserTextInput />
+        <UserTextInput />
+        <UserTextInput />
+        <UserTextInput />
+        <UserTextInput />
+        <UserTextInput />
+        <UserTextInput />
+        <UserTextInput />
+      </ScrollView>
+      <View style={styles.btnContainer}>
+        <Buttons />
+      </View>
+    </View>
+  );
+}
