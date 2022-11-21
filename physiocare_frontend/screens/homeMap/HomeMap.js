90fc2b2e244c4e226d37db72e53c945e9ec6f3ed
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 16:24:31 +0200

    frontend -> importing useState and useEffect from react in HomeMap.js

--- physiocare_frontend/screens/homeMap/HomeMap.js
+++ physiocare_frontend/screens/homeMap/HomeMap.js
@@ -1,3 +1,4 @@
 import * as React from "react";
 import { View, StyleSheet, Text, Dimensions } from "react-native";
+import { useState, useEffect } from "react";
 import Buttons from "../../components/button/Buttons";
