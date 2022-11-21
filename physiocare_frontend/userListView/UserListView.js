b9babba428727bfa389ad6a69efced6f4c4689df
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 17:28:14 +0200

    frontend -> importing AsyncStorage in UserListView.js

--- physiocare_frontend/userListView/UserListView.js
+++ physiocare_frontend/userListView/UserListView.js
@@ -3,3 +3,4 @@ import axios from "axios";
 import { View, StyleSheet, ScrollView, Text } from "react-native";
 import UserCard from "../../components/UserCard";
 import SearchingBar from "../../components/SearchingBar";
+import AsyncStorage from "@react-native-async-storage/async-storage";
