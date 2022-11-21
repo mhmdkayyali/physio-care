8ae88d314c548338e21c5f8c26ab2e9fa7d588a0
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 20:59:05 +0200

    frontend -> importing calender from react native calender in ScheduleDay.js

--- physiocare_frontend/screens/bookingSession/scheduleDay/ScheduleDay.js
+++ physiocare_frontend/screens/bookingSession/scheduleDay/ScheduleDay.js
@@ -1,3 +1,4 @@
 import { StyleSheet, View, Text } from "react-native";
 import Buttons from "../../components/Buttons";
 import ProfilePicture from "../../components/ProfilePicture";
+import { Calendar } from "react-native-calendars";
