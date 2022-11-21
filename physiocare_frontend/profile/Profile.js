89271660a58b3e317c7f329ac098c9fef5ea7e17
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 20:14:36 +0200

    frontend -> importing asyncStorage in Profile.js

--- physiocare_frontend/profile/Profile.js
+++ physiocare_frontend/profile/Profile.js
@@ -3,3 +3,4 @@ import ProfileInput from "../components/ProfileInput";
 import ProfilePicture from "../components/ProfilePicture";
 import Buttons from "../components/Buttons";
 import { View, StyleSheet, ScrollView } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
