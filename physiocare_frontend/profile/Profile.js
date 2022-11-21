bb7157cb4ae9f2329d1481fa32658e4cbebd1c84
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 20:13:29 +0200

    frontend -> importing Buttons component in Profile.js

--- physiocare_frontend/profile/Profile.js
+++ physiocare_frontend/profile/Profile.js
@@ -1,3 +1,4 @@
 import ProfileInput from "../components/ProfileInput";
 import ProfilePicture from "../components/ProfilePicture";
+import Buttons from "../components/Buttons";
 import { View, StyleSheet, ScrollView } from "react-native";
