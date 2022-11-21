fe5de8c276194fb2096f241d11aac0fd5ac02289
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 08:46:18 +0200

    frontend -> styling UserTextInput.js

--- physiocare_frontend/userTextInput/UserTextInput.js
+++ physiocare_frontend/userTextInput/UserTextInput.js
@@ -14,3 +14,14 @@ function UserTextInput(props) {
 }
 
 export default UserTextInput;
+
+const styles = StyleSheet.create({
+  textInput: {
+    width: "100%",
+    backgroundColor: "#FFFFFF",
+    height: 48,
+    borderRadius: 5,
+    marginBottom: 16,
+    paddingLeft: 12,
+  },
+});
