a620b6f67dc199b580d1695ec7e466cdecd1057a
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 21:21:40 +0200

    frontend -> importing ProfilePicture component in ScheduleTime.js

--- physiocare_frontend/screens/bookingSession/scheduleTime/ScheduleTime.js
+++ physiocare_frontend/screens/bookingSession/scheduleTime/ScheduleTime.js
@@ -1,3 +1,4 @@
 import { StyleSheet, View, Text } from "react-native";
 import { useState, useEffect } from "react";
 import Btn from "../../components/Btn";
+import ProfilePicture from "../../components/ProfilePicture";
