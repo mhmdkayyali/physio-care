77e074cee94122cfdbefd88dc6943aaaac859c3a
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 13:50:59 +0200

    frontend -> importing userTextInput and Buttons from components in SignupTwo.js

--- physiocare_frontend/screens/signup/therapistSignup/signupTwo/SignupTwo.js
+++ physiocare_frontend/screens/signup/therapistSignup/signupTwo/SignupTwo.js
@@ -1,1 +1,3 @@
+import UserTextInput from "../../../components/UserTextInput";
 import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import Buttons from "../../../components/Buttons";
