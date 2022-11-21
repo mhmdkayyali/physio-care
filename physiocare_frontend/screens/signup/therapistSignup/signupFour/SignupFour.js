10fd6dff9ef18fc3e0109eb306f0b1882f396270
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 15:36:51 +0200

    frontend -> importing useState and useEffect from react in SignupFour.js

--- physiocare_frontend/screens/signup/therapistSignup/signupFour/SignupFour.js
+++ physiocare_frontend/screens/signup/therapistSignup/signupFour/SignupFour.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View, Image, Pressable, Button } from "react-native";
 import Buttons from "../../../components/Buttons";
 import TimeInput from "../../../components/TimeInput";
