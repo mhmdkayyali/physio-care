643ea7cad42c7463cce0068a12a4fe0a4db10d7b
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-21 12:59:02 +0200

    frontend -> importing axiox and mapView in SignupFour.js

--- physiocare_frontend/screens/signup/patientSignup/signupFour/SignupFour.js
+++ physiocare_frontend/screens/signup/patientSignup/signupFour/SignupFour.js
@@ -1,2 +1,4 @@
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
 import { useEffect, useState } from "react";
+import axios from "axios";
 import { StyleSheet, Text, View, Image, Pressable } from "react-native";
