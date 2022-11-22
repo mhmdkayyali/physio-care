cc9b214d62e9cea69ab52bd7532a49d05f85bb10
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-22 20:48:33 +0200

    frontend -> importing navigation container and createNativeStackNavigator

---
+++ physiocare_frontend/navigation/stack/Stack.js
@@ -0,0 +1,4 @@
+import * as React from "react";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
