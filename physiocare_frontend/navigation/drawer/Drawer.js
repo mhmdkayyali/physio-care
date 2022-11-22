0fbaaa80d4f3e1140c9bf2711d3d95fd8b5dca13
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-22 20:15:10 +0200

    frontend -> adding Appointment drawer screen in Drawer.js

--- physiocare_frontend/navigation/drawer/Drawer.js
+++ physiocare_frontend/navigation/drawer/Drawer.js
@@ -3,6 +3,7 @@ import "react-native-gesture-handler";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import HomeMap from "../../screens/homeMap/HomeMap";
+import Appointment from "../../screens/appointment/Appointment";
 import Profile from "../../screens/profile/Profile";
 
 const Drawer = createDrawerNavigator();
@@ -25,6 +26,7 @@ const DrawerNavigator = () => {
     >
       <Drawer.Screen name="HomeMap" component={HomeMap} />
       <Drawer.Screen name="Profile" component={Profile} />
+      <Drawer.Screen name="Appointment" component={Appointment} />
     </Drawer.Navigator>
   );
 };
