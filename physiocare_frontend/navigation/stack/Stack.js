2b6322ca4c2778645be161af6f090cc4afb52b76
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-22 20:49:25 +0200

    frontend -> adding stack navigation  and putting initial route name

--- physiocare_frontend/navigation/stack/Stack.js
+++ physiocare_frontend/navigation/stack/Stack.js
@@ -2,3 +2,20 @@ import * as React from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+const NativeStackNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerStyle: { backgroundColor: "#1A7C6B" },
+          headerTitleAlign: "center",
+          headerTintColor: "#FFFFFF",
+        }}
+      ></Stack.Navigator>
+    </NavigationContainer>
+  );
+};
