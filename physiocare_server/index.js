2a2872f0a70c454592e6cf8a2cd86eca5d73b89a
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-22 23:01:41 +0200

    backend -> listening to port in index.js

--- physiocare_server/index.js
+++ physiocare_server/index.js
@@ -28,3 +28,10 @@ const linkRoutes = require("./routes/links.routes");
 app.use("/link", linkRoutes);
 
 require("./config/db.config");
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server running on port ${process.env.PORT}`);
+  }
+});
