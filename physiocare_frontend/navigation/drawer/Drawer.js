42942e3564d78ca19505aa86c7149b9a998ed3cb
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-22 20:11:15 +0200

    frontend -> adding drawer navigator and screenoptions for styling in Drawer.js

--- physiocare_frontend/navigation/drawer/Drawer.js
+++ physiocare_frontend/navigation/drawer/Drawer.js
@@ -3,3 +3,15 @@ import "react-native-gesture-handler";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerTitleAlign: "center",
+        drawerActiveTintColor: "#35DB9F",
+        headerTintColor: "#FFFFFF",
+      }}
+    ></Drawer.Navigator>
+  );
+};
