1144856a9c302b24a1bacf64ee12a5280fdda347
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-23 14:19:36 +0200

    backend -> adding getAllPatients api in patient controller

--- physiocare_server/controllers/patient.controller.js
+++ physiocare_server/controllers/patient.controller.js
@@ -1,1 +1,18 @@
 const db = require("../config/db.config");
+
+const getAllPatients = async (req, res) => {
+  try {
+    const patients = await db.users.findMany({
+      where: {
+        user_type: "PATIENT",
+      },
+      include: {
+        pt_additional_informations: true,
+      },
+    });
+
+    return res.json(patients);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
