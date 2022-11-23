3b6a032a90aac0cb450b745ff762b0b01ecd152b
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-23 12:02:05 +0200

    backend -> listening to port and seeting route

--- physiocare_server/videosdk/index.js
+++ physiocare_server/videosdk/index.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.listen(3000, () => {
+  console.log("Application started and Listening on port 3000");
+});
+
+app.get("/video", (req, res) => {
+  res.sendFile(`index.html`, { root: __dirname });
+});
