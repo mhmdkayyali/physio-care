1df798f893fcca93845230f5338e7aaa21515081
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-23 14:54:20 +0200

    backend -> adding getLink api in link controller

--- physiocare_server/controllers/link.controller.js
+++ physiocare_server/controllers/link.controller.js
@@ -8,3 +8,17 @@ const getAllLinks = async (req, res) => {
     res.send(e.message);
   }
 };
+
+const getLink = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const link = await db.meeting_links.findFirst({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    return res.json(link);
+  } catch (e) {
+    res.send(e.message);
+  }
+};
