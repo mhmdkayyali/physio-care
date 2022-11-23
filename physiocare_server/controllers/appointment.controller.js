c8b897c3ed08b90e6ca6e02e6cb9ac3f714e1fb4
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-23 15:02:54 +0200

    backend -> adding getAllAppointments api in appointment controller

--- physiocare_server/controllers/appointment.controller.js
+++ physiocare_server/controllers/appointment.controller.js
@@ -1,1 +1,14 @@
 const db = require("../config/db.config");
+
+const getAllAppointments = async (req, res) => {
+  try {
+    const appointment = await db.appointments.findMany({
+      include: {
+        meeting_links: true,
+      },
+    });
+    return res.json(appointment);
+  } catch (e) {
+    res.send(e.message);
+  }
+};
