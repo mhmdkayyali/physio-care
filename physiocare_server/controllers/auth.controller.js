6acc6dbf48b4251e8a34701b55708cc8946aba9b
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-23 15:51:45 +0200

    backend -> adding login api in auth controller

--- physiocare_server/controllers/auth.controller.js
+++ physiocare_server/controllers/auth.controller.js
@@ -1,3 +1,34 @@
 const db = require("../config/db.config");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await db.users.findFirst({
+      where: {
+        email,
+      },
+      include: {
+        pt_additional_informations: true,
+        therapist_additional_informations: true,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).send("Invalid credentials");
+    }
+
+    const checkPass = await bcrypt.compare(password, user.password);
+
+    if (!checkPass) {
+      return res.status(400).send("Invalid credentials");
+    }
+
+    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET_KEY);
+
+    return res.json({ user, token });
+  } catch (err) {
+    console.log(err);
+  }
+};
