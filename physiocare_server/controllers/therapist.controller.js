02696b9e122005f8bef67d58f85c53e514efe1a9
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-23 14:38:35 +0200

    backend -> adding getAllTherapists api in therapist controller

--- physiocare_server/controllers/therapist.controller.js
+++ physiocare_server/controllers/therapist.controller.js
@@ -1,1 +1,18 @@
 const db = require("../config/db.config");
+
+const getAllTherapists = async (req, res) => {
+  try {
+    const therapists = await db.users.findMany({
+      where: {
+        user_type: "THERAPIST",
+      },
+      include: {
+        therapist_additional_informations: true,
+      },
+    });
+
+    return res.json(therapists);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+};
