205a36e743b5264a83ffa32fa969b0d367c2c795
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-23 14:30:41 +0200

    backend -> requiring patient controller

--- physiocare_server/routes/patients.routes.js
+++ physiocare_server/routes/patients.routes.js
@@ -1,1 +1,11 @@
 const { Router } = require("express");
+const router = Router();
+const {
+  getAllPatients,
+  getPatient,
+  updatePatient,
+  deletePatient,
+  createAppointment,
+  cancelAppointment,
+  getAppointments,
+} = require("../controllers/patient.controller");
