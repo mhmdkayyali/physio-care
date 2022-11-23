b4b4db11b987e687afc9563ea977296535dd77e6
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-23 12:31:04 +0200

    backend -> adding WebView with its options

--- physiocare_frontend/screens/videoCall/VideoCall.js
+++ physiocare_frontend/screens/videoCall/VideoCall.js
@@ -2,3 +2,22 @@ import React, { useEffect } from "react";
 import { Text } from "react-native";
 import { WebView } from "react-native-webview";
 import { Camera } from "expo-camera";
+
+const VideoPage = ({ navigation, route }) => {
+  return (
+    <WebView
+      originWhitelist={["*"]}
+      incognito={true}
+      allowsInlineMediaPlayback
+      javaScriptEnabled
+      scalesPageToFit
+      mediaPlaybackRequiresUserAction={true}
+      javaScriptEnabledAndroid
+      useWebkit
+      startInLoadingState={true}
+      source={{
+        uri: `http://192.168.43.32:3000/video?meetingId=123123&name=name`,
+      }}
+    />
+  );
+};
