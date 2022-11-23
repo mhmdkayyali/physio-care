4c78fdf158a336e75dd98c77d804df441edd0eb1
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-23 14:39:08 +0200

    backend -> adding getTherapist api in therapist controller

--- physiocare_server/controllers/therapist.controller.js
+++ physiocare_server/controllers/therapist.controller.js
@@ -16,3 +16,21 @@ const getAllTherapists = async (req, res) => {
     res.status(500).send(e);
   }
 };
+
+const getTherapist = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const therapist = await db.users.findFirst({
+      where: {
+        user_type: "THERAPIST",
+        id: parseInt(id),
+      },
+      include: {
+        therapist_additional_informations: true,
+      },
+    });
+    return res.json(therapist);
+  } catch (e) {
+    res.send(e.message);
+  }
+};
