35d60133eb70035c0b2b1f3310328c24a4f541fc
Mohammad Al Kayyali <mohammad.a.kayyali@gmail.com>
2022-11-25 00:38:41 +0200

    backend -> editting files

--- physiocare_server/middlewares/auth.middleware.js
+++ physiocare_server/middlewares/auth.middleware.js
@@ -5,14 +5,6 @@ const authMiddleware = async (req, res, next) => {
   if (!token) return res.status(401).json({ message: "Unauthorized" });
   try {
     const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
-
-    const admin = await db.users.findFirst({
-      where: {
-        email: decode.email,
-      },
-    });
-
-    req.user = admin;
     next();
   } catch (err) {
     return res.status(401).json({ message: "Unauthorized" });
